ab59872ae819b824d7e51ce61b4b1553dd17e0cc
Arsen Dadunts <adadunts-ext@beachbody.com>
2022-04-08 17:35:03 +0400

    Updated CucumberRunnerTest class

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -1,7 +1,12 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "", features = "src/test/resources/features", glue = "goRest")
+@CucumberOptions(
+        tags = "",
+        features = "src/test/resources/features",
+        glue = "goRest",
+        monochrome = true,
+        plugin= {"pretty","json:target/cucumber/cucumber.json"})
 
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
 
