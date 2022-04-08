a8f8d67cb544849dbfe5d8e6957f80407ecc9a05
Arsen Dadunts <adadunts-ext@beachbody.com>
2022-04-08 17:33:32 +0400

    Added variables to Constants class

--- src/main/java/goRest/common/Constants.java
+++ src/main/java/goRest/common/Constants.java
@@ -1,6 +1,17 @@
 package goRest.common;
 
 public class Constants {
+    //API
+    public static final String API_RESPONSE_CODE = "API_RESPONSE_CODE";
+    public static final String API_RESPONSE_BODY = "API_RESPONSE_BODY";
+    public static final String OPTIONS = "OPTIONS";
+    public static final String DELETE = "DELETE";
+    public static final String GET = "GET";
+    public static final String PUT = "PUT";
+    public static final String PATCH = "PATCH";
+    public static final String POST = "POST";
+
+    //GoREST
     public static final String BASE_URL = "https://gorest.co.in/public/v2";
     public static final String ACCESS_TOKEN = "77ffc4ac345898a3f7a0eee29c2e2c0081477541b2b88deacb7d68b2fac0ab28";
     public static final String USER_BY_ID = "users/{userId}";
